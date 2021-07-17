1227dfc5bc127d16699200302da3acf1b76f1f97
Frank Tang <frank.tang@instacart.com>
2021-07-17 12:07:31 -0400

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_154628) do
+ActiveRecord::Schema.define(version: 2021_07_17_160513) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
