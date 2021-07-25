b99a69cf6b863db3424b8daf423e1f71247dd88d
Frank Tang <frank.tang@instacart.com>
2021-07-24 23:58:19 -0400

    Add association with category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
   def new
     @category = Category.new
   end
@@ -9,6 +11,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
@@ -21,8 +24,29 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def require_admin
+      if !(logged_in? && current_user.admin?)
+        flash[:alert] = "Only admin can perform that action"
+        redirect_to categories_path
+      end
     end
 end
